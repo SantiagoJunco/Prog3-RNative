c39030c8591844c740558f38f075a1a8bd4e17e7
SantiagoJunco <Jsantiagoemilio@udesa.edu.ar>
2023-11-06 08:38:01 -0300

    correción mostrar comentarios

--- src/screens/Comments.js
+++ src/screens/Comments.js
@@ -4,39 +4,48 @@ import FormComentario from '../components/FormComentario'
 import { db } from '../firebase/config'
 
 export default class Comments extends Component {
-    constructor(props){
+    constructor(props) {
         super(props)
         this.state = {
             post: null
         }
     }
-    
-    componentDidMount(){
+
+    componentDidMount() {
+        console.log(this.props)
         db
-        .collection('posts')
-        .doc(this.props.route.params.id)
-        .onSnapshot((doc)=>{
-            if(doc){
-            this.setState({post: doc.data()})
-            }
-        })
-        console.log(this.state.post);
+            .collection('posts')
+            .doc(this.props.route.params.id)
+            .onSnapshot((doc) => {
+                if (doc) {
+                    this.setState({ post: doc.data() })
+                }
+            })
     }
-  render() {
-    return (
-        <View>
-        <Text>Comments</Text>
-        {
-            
-                this.state.post !== null ?          
-                    console.log(this.state.post)
-                :
-                <Text> Aún no hay comentarios en este post</Text>
-            }
-        <FormComentario
-         postId= {this.props.route.params.id}
-        />
-      </View>
-    )
-  }
+    render() {
+        return (
+            <View>
+                <Text>Comments</Text>
+                {
+
+                    this.state.post !== null ?
+                        this.state.post.comentarios.length !== 0 ?
+                            <FlatList
+                                data={this.state.post.comentarios}
+                                keyExtractor={(item) => item.createdAt.toString()}
+                                renderItem={({ item }) => <View>
+                                    <Text>{item.owner}</Text>
+                                    <Text>{item.comentario}</Text>
+                                </View>} />
+                            :
+                            <Text> Aún no hay comentarios en este post</Text>
+                        :
+                        ''
+                }
+                <FormComentario
+                    postId={this.props.route.params.id}
+                />
+            </View>
+        )
+    }
 }
