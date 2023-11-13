f4743cf76e3300d2f87a67f4cf89ba4e31c0f434
luchosetton <lsetton@udesa.edu.ar>
2023-11-13 09:27:29 -0300

    screen login

--- src/screens/Login.js
+++ src/screens/Login.js
@@ -1,39 +1,82 @@
-import { Text, View, StyleSheet, TouchableOpacity } from 'react-native'
-import React, { Component } from 'react'
-import FormLogin from '../components/FormLogin'
-import { auth } from '../firebase/config'
+import React, { Component } from 'react';
+import { Text, View, StyleSheet, TouchableOpacity, ActivityIndicator } from 'react-native';
+import FormLogin from '../components/FormLogin';
+import { auth } from '../firebase/config';
 
 export default class Login extends Component {
-  constructor(props){
-    super(props)
-  }
+    constructor(props) {
+        super(props);
+        this.state = {
+            loading: true, // Electiva loader
+        };
+    }
 
-  componentDidMount(){
-    auth.onAuthStateChanged(( user )=> {
-      if(user !== null){
-        this.props.navigation.navigate('TabNavigation')
-      }
-    })
-  }
+    componentDidMount() {
+        auth.onAuthStateChanged((user) => {
+            this.setState({ loading: false });
 
-  render() {
-    return (
-      <View style={styles.container}>
-        <FormLogin navigation={this.props.navigation} />
-        <Text>
-          ¿Aún no tienes una cuenta? 
-          <TouchableOpacity
-            onPress={()=> this.props.navigation.navigate('Register')}
-          >Registrate aquí!</TouchableOpacity>
-        </Text>
-      </View>
-    )
-  }
+            if (user !== null) {
+                this.props.navigation.navigate('TabNavigation');
+            }
+        });
+    }
+
+    render() {
+        if (this.state.loading) {
+            return (
+                <View style={styles.loaderContainer}>
+                    <ActivityIndicator size="large" color="blue" />
+                </View>
+            );
+        }
+
+        return (
+            <View style={styles.container}>
+                <View style={styles.formContainer}>
+                    <FormLogin navigation={this.props.navigation} />
+                </View>
+                <View style={styles.textContainer}>
+                    <Text style={styles.text}>¿Aún no tienes una cuenta?</Text>
+                    <TouchableOpacity onPress={() => this.props.navigation.navigate('Register')}>
+                        <Text style={styles.link}>Regístrate aquí!</Text>
+                    </TouchableOpacity>
+                </View>
+            </View>
+        );
+    }
 }
 
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        justifyContent:'center'
-    }
-})
+        justifyContent: 'center',
+        alignItems: 'center',
+        padding: 20,
+        backgroundColor: '#2980B9',
+    },
+    formContainer: {
+        width: '100%',
+        marginBottom: 20,
+    },
+    textContainer: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        marginTop: 20,
+    },
+    text: {
+        color: 'white',
+        fontWeight: 'bold',
+        fontSize: 16,
+    },
+    link: {
+        color: 'yellow',
+        fontSize: 16,
+        fontWeight: 'bold',
+        marginLeft: 5,
+    },    
+    loaderContainer: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+});
