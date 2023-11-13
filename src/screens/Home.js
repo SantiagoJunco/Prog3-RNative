c73d18279d7a431e6d5f521f3d2d0227681e60b0
luchosetton <lsetton@udesa.edu.ar>
2023-11-13 09:29:35 -0300

    screen home

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,67 +1,64 @@
-import { Text, View, TouchableOpacity, StyleSheet, Image, FlatList, ActivityIndicator } from 'react-native'
-import React, { Component } from 'react'
-import { db } from '../firebase/config'
-import Post from '../components/Post'
-
+import React, { Component } from 'react';
+import { View, FlatList, StyleSheet, Text } from 'react-native';
+import Post from '../components/Post';
+import { db } from '../firebase/config';
 
 export default class Home extends Component {
-    constructor(props){
-        super(props)
-        this.state ={
-            posteos: []
-        }
-    }  
+    constructor(props) {
+        super(props);
+        this.state = {
+            posteos: [],
+        };
+    }
 
-    componentDidMount(){
+    componentDidMount() {
         db.collection('posts')
-        .orderBy('createdAt', 'desc')
-        .limit(6)
-        .onSnapshot(docs => {
-            let arrPosteos = []
-            docs.forEach(doc => {
-                arrPosteos.push({
-                    id: doc.id,
-                    data: doc.data()
-                })
-            })
+            .orderBy('createdAt', 'desc')
+            .limit(6)
+            .onSnapshot((docs) => {
+                let arrPosteos = [];
+                docs.forEach((doc) => {
+                    arrPosteos.push({
+                        id: doc.id,
+                        data: doc.data(),
+                    });
+                });
 
-            this.setState({
-               posteos: arrPosteos 
-            })
-        })
+                this.setState({
+                    posteos: arrPosteos,
+                });
+            });
     }
 
     render() {
         return (
-            <View>
+            <View style={styles.container}>
+                <View style={styles.pageTitleContainer}>
+                    <Text style={styles.pageTitle}>ReSport: Remeras Usadas</Text>
+                </View>
                 <FlatList
                     data={this.state.posteos}
-                    keyExtractor={(item)=> item.id.toString()}
-                    renderItem={({ item })=> <Post navigation={this.props.navigation} data={item.data} id={item.id}/>}
+                    keyExtractor={(item) => item.id.toString()}
+                    renderItem={({ item }) => <Post navigation={this.props.navigation} data={item.data} id={item.id} />}
                 />
-                
             </View>
-        )
+        );
     }
 }
 
-const styles = StyleSheet.create(
-    {
-        containerGral:{
-            flex:1
-        },
-        container : {
-            flex : 2,
-            alignContent:'centrer'
-    
-        },
-        containerGrande:{
-            flex:3,
-            backgroundColor:'blue'
-        },
-        containerChico:{
-            flex:1,
-            backgroundColor:'orange'
-        }
-    }
-) 
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        padding: 20,
+        backgroundColor: '#2980B9',
+    },
+    pageTitleContainer: {
+        alignItems: 'center',
+    },
+    pageTitle: {
+        fontSize: 24,
+        fontWeight: 'bold',
+        marginBottom: 20,
+        color: '#fff'
+    },
+});
